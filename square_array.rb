37f9fe5c27fbaacd1f688aba9f5576bca1e602a8
William Bainton <baintonw@gmail.com>
2019-05-03 23:37:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,18 @@
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   array.each do |element|
     element**2
     new_array.push(element**2)
   end
   return new_array
+=======
+  array = [1, 2, 3]
+  array.each do |number|
+    number ** 2
+    number += 1
+  end
+  return new_array
+  
+>>>>>>> 67cae43514caec37df4169e26d7300a2dbecd1a5
 end
